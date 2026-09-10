0b109527686a267e0c478346ba40c81ca34d0108
sobreruedasesm <aarboleda_darwin@hotmail.com>
2026-09-10 16:11:47 -0500

    Initialize Supabase client in supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://rrfmgtvcglxvequkwist.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_Rz-D_jPU9sODNl-YbBmPOQ_gdTDUeXy";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
