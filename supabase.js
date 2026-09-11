fb6ec97849ab5940869450fa582f7c7356b69761
sobreruedasesm <aarboleda_darwin@hotmail.com>
2026-09-10 22:34:54 -0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,4 @@
 const SUPABASE_URL = "https://rrfmgtvcglxvequkwist.supabase.co";
-
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_Rz-D_jPU9sODNl-YbBmPOQ_gdTDUeXy";
 
 const supabaseClient = supabase.createClient(
